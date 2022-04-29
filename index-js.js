0982e332c6da9af4887238d17959ccb257422014
Eric Furrevig <efurrevig@gmail.com>
2022-04-29 13:33:31 -0400

    Added most function and numbers buttons, display

--- index-js.js
+++ index-js.js
@@ -15,5 +15,14 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
+    if (operator === 'add') {
+        return add(a,b);
+    } else if (operator === 'subtract') {
+        return subtract(a,b);
+    } else if (operator === 'multiply') {
+        return multiply(a,b);
+    } else if (operator === 'divide') {
+        return divide(a,b);
+    }
 
 }
